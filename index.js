a70f10e2915da521597d868bca6c79339ddf1fe2
Abdullah-Younus <abdullah1001k@gmail.com>
2022-12-31 00:13:38 +0500

    student

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
 class Person {
-    name: string;
-    age: number;
-    constructor(name: string, age: number) {
+    name;
+    age;
+    constructor(name, age) {
         this.name = name;
         this.age = age;
     }
     getName() {
-        return this.name
+        return this.name;
     }
 }
-
 class Student extends Person {
-    rollNumber: string;
-    courses: string[] = []
-    constructor(name: string, age: number, rollNumber: string) {
+    rollNumber;
+    courses = [];
+    constructor(name, age, rollNumber) {
         super(name, age);
         this.rollNumber = rollNumber;
     }
-    registerForCourses(course: string) {
-        this.courses.push(course)
+    registerForCourses(course) {
+        this.courses.push(course);
     }
 }
-
 const student1 = new Student("Abdullah", 45, "5726975");
 student1.registerForCourses("BSC");
 student1.registerForCourses("Metaverse");
-
 console.log('Student1 ==>', student1);
-
 const student2 = new Student("Sameer", 25, "4546465465");
 student2.registerForCourses('BlockChain');
-console.log('student2 ===>',student2);
+console.log('student2 ===>', student2);
+export {};
